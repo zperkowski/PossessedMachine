7129a710f136a4a8551315c2b1cb4e166a16a388
zperkowski <ZdzislawPerkowski@outlook.com>
2017-09-12 15:28:35 +0200

    Creating a configuration window

--- src/com/zperkowski/PossessedMachine/MainWindowController.java
+++ src/com/zperkowski/PossessedMachine/MainWindowController.java
@@ -1,4 +1,25 @@
 package com.zperkowski.PossessedMachine;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainWindowController {
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private void menuBar_File_Close_Clicked() {
+
+    }
+
+    @FXML
+    private void menuBar_Help_Help_Clicked() {
+
+    }
+
+    @FXML
+    private void menuBar_Help_About_Clicked() {
+
+    }
 }
