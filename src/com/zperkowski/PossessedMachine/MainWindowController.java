f409fb0ccd7f42dc679f3b2564fcd49e3150f3e1
zperkowski <ZdzislawPerkowski@outlook.com>
2017-09-25 19:43:12 +0200

    Changing language at the runtime
    Added english and polish

--- src/com/zperkowski/PossessedMachine/MainWindowController.java
+++ src/com/zperkowski/PossessedMachine/MainWindowController.java
@@ -3,10 +3,26 @@ package com.zperkowski.PossessedMachine;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.util.Locale;
+
 public class MainWindowController {
 
     @FXML
-    private Label infoLabel;
+    private Menu menu_file;
+    @FXML
+    private MenuItem menu_file_close;
+    @FXML
+    private Menu menu_language;
+    @FXML
+    private RadioMenuItem menu_language_english;
+    @FXML
+    private RadioMenuItem menu_language_polish;
+    @FXML
+    private Menu menu_help;
+    @FXML
+    private MenuItem menu_help_help;
+    @FXML
+    private MenuItem menu_help_about;
     @FXML
     private TextField textField_IP_static;
     @FXML
@@ -16,13 +32,44 @@ public class MainWindowController {
     @FXML
     private TableColumn tableColumn_IP_IP;
     @FXML
+    private Button button_scanNetwork;
+    @FXML
+    private Button button_connect;
+    @FXML
+    private Button button_monitorPosition;
+    @FXML
     private CheckBox checkBox_autostart;
     @FXML
     private CheckBox checkBox_autoconnect;
+    @FXML
+    private Label infoLabel;
 
     @FXML
     private void initialize() {
+        bindLanguage();
         adjustColumnsWidth();
+    }
+
+    /**
+     * Changes language to users choice.
+     */
+    private void bindLanguage() {
+        menu_file.textProperty().bind(LanguageChanger.createStringBinding("file"));
+        menu_file_close.textProperty().bind(LanguageChanger.createStringBinding("close"));
+        menu_language.textProperty().bind(LanguageChanger.createStringBinding("language"));
+        menu_language_english.textProperty().bind(LanguageChanger.createStringBinding("english"));
+        menu_language_polish.textProperty().bind(LanguageChanger.createStringBinding("polish"));
+        menu_help.textProperty().bind(LanguageChanger.createStringBinding("help"));
+        menu_help_help.textProperty().bind(LanguageChanger.createStringBinding("help"));
+        menu_help_about.textProperty().bind(LanguageChanger.createStringBinding("about"));
+        textField_IP_static.tooltipProperty().set(LanguageChanger.tooltipForKey("ip_address"));
+        tableColumn_IP_name.textProperty().bind(LanguageChanger.createStringBinding("ip_name"));
+        tableColumn_IP_IP.textProperty().bind(LanguageChanger.createStringBinding("ip_address"));
+        button_scanNetwork.textProperty().bind(LanguageChanger.createStringBinding("scan"));
+        button_connect.textProperty().bind(LanguageChanger.createStringBinding("connect"));
+        button_monitorPosition.textProperty().bind(LanguageChanger.createStringBinding("monitor_position"));
+        checkBox_autostart.textProperty().bind(LanguageChanger.createStringBinding("autostart"));
+        checkBox_autoconnect.textProperty().bind(LanguageChanger.createStringBinding("autoconnect"));
     }
 
     /**
@@ -71,5 +118,19 @@ public class MainWindowController {
     @FXML
     private void checkBox_autoconnect_clicked() {
 
+    }
+
+    @FXML
+    private void radioMenu_english_selected() {
+        switchLanguage(LanguageChanger.ENGLISH);
+    }
+
+    @FXML
+    private void radioMenu_polish_selected() {
+        switchLanguage(LanguageChanger.POLISH);
+    }
+
+    private void switchLanguage(Locale locale) {
+        LanguageChanger.setLocale(locale);
     }
 }
